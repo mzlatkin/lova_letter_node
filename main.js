6f115e0f883c821b2fcf35fc29d86c0470de047b
Martin Zlatkin <mzlatkin@elltechnologies.com>
2018-02-02 16:00:42 -0500

    added files

--- main.js
+++ main.js
@@ -94,4 +94,35 @@ function game_model(obj)
 		alert("game_over");
 	}
 	obj.add_player();
+
+
+
+	var joined = ko.observable(false);
+
+    socket.on("room_test", function(data) {
+        // self.get_all_characters_success(data)
+        console.log(data);
+    })
+
+    self.join_room_1 = function(name)
+    {
+        console.log("joined_room_1")
+        socket.emit("join_room", "room1");
+        ready = true;
+        self.joined(true);
+    }
+
+    self.join_room_2 = function(name)
+    {
+        console.log("joined_room_2")
+        socket.emit("join_room", "room2");
+        ready = true;
+        self.joined(true);
+    }
+
+    self.test_room_1 = function(name)
+    {
+        socket.emit("test_room", "room1");
+    }
+    
 }
