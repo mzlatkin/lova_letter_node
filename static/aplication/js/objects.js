59c824ad890033fd3c7697367ee60467238ff8a2
Martin Zlatkin <mzlatkin@elltechnologies.com>
2018-02-02 16:08:39 -0500

    added files

--- static/aplication/js/objects.js
+++ static/aplication/js/objects.js
@@ -9,34 +9,6 @@ function build_game_viewModel()
     }
 
     game_model(self);
-
-    obj.joined = ko.observable(false);
-
-    socket.on("room_test", function(data) {
-        // self.get_all_characters_success(data)
-        console.log(data);
-    })
-
-    self.join_room_1 = function(name)
-    {
-        console.log("joined_room_1")
-        socket.emit("join_room", "room1");
-        ready = true;
-        self.joined(true);
-    }
-
-    self.join_room_2 = function(name)
-    {
-        console.log("joined_room_2")
-        socket.emit("join_room", "room2");
-        ready = true;
-        self.joined(true);
-    }
-
-    self.test_room_1 = function(name)
-    {
-        socket.emit("test_room", "room1");
-    }
 }
 
 var player_model = function(data)
