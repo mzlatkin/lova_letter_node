777379a5a775990d2a977dd8976e140e5822cf03
Martin Zlatkin <mzlatkin@elltechnologies.com>
2018-02-02 15:58:36 -0500

    added files

--- static/aplication/js/utils.js
+++ static/aplication/js/utils.js
@@ -40,14 +40,99 @@ function ajax_get_player_template()
 
 function ajax_get_cards()
 {
-    var ret = undefined;
-    $.ajax({
-        url: "assets/models/cards.JSON",
-        dataType: "json",
-        async: false,
-        success: function (data, status, xhr) {
-            ret = data;
-        }
-    });
+    // var ret = undefined;
+    // $.ajax({
+    //     url: "assets/models/cards.JSON"+'?ts='+new Date().getTime(),
+    //     dataType: "json",
+    //     async: false,
+    //     success: function (data, status, xhr) {
+    //         ret = data;
+    //     }
+    // });
+    // return ret;
+
+    ret = {
+        "cards": [{
+            "name": "Guard",
+            "value": 1,
+            "text":"Pick a player and a Non-Guard card",
+            "img":"assets/images/Guard.png"
+        }, {
+            "name": "Guard",
+            "value": 1,
+            "text":"Pick a player and a Non-Guard card",
+            "img":"assets/images/Guard.png"
+        }, {
+            "name": "Guard",
+            "value": 1,
+            "text":"Pick a player and a Non-Guard card",
+            "img":"assets/images/Guard.png"
+        }, {
+            "name": "Guard",
+            "value": 1,
+            "text":"Pick a player and a Non-Guard card",
+            "img":"assets/images/Guard.png"
+        }, {
+            "name": "Guard",
+            "value": 1,
+            "text":"Pick a player and a Non-Guard card",
+            "img":"assets/images/Guard.png"
+        }, {
+            "name": "Preist",
+            "value": 2,
+            "text":"Pick a Player, see their card",
+            "img":"assets/images/Preist.png"
+        }, {
+            "name": "Preist",
+            "value": 2,
+            "text":"Pick a Player, see their card",
+            "img":"assets/images/Preist.png"
+        }, {
+            "name": "Barron",
+            "value": 3,
+            "text":"Compare cards with another player",
+            "img":"assets/images/Barron.png"
+        }, {
+            "name": "Barron",
+            "value": 3,
+            "text":"Compare cards with another player",
+            "img":"assets/images/Barron.png"
+        }, {
+            "name": "Hand Maiden",
+            "value": 4,
+            "text":"safe from all targeting cards",
+            "img":"assets/images/Hand Maiden.png"
+        }, {
+            "name": "Hand Maiden",
+            "value": 4,
+            "text":"safe from all targeting cards",
+            "img":"assets/images/Hand Maiden.png"
+        }, {
+            "name": "Prince",
+            "value": 5,
+            "text":"Choose a player, that player discards a card and picks up a new one",
+            "img":"assets/images/Prince.png"
+        }, {
+            "name": "Prince",
+            "value": 5,
+            "text":"Choose a player, that player discards a card and picks up a new one",
+            "img":"assets/images/Prince.png"
+        }, {
+            "name": "King",
+            "value": 6,
+            "text":"Swap hands with another player",
+            "img":"assets/images/King.png"
+        }, {
+            "name": "Countess",
+            "value": 7,
+            "text":"if you are caught with the countess and a king or prince you must disscard the king/prince",
+            "img":"assets/images/Countess.png"
+        }, {
+            "name": "Princess",
+            "value": 8,
+            "text":"If you discard this card you lose",
+            "img":"assets/images/Princess.png"
+        }]
+    }
     return ret;
 }
