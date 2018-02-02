2c24ad9d433acb67b86c9df60a1872ff4163e0f1
Martin Zlatkin <mzlatkin@elltechnologies.com>
2018-02-02 15:43:27 -0500

    added files

--- static/aplication/js/utils.js
+++ static/aplication/js/utils.js
@@ -40,6 +40,7 @@ function ajax_get_player_template()
 
 function ajax_get_cards()
 {
+    console.log("called ajax");
     var ret = undefined;
     $.ajax({
         url: "assets/models/cards.JSON"+'?ts='+new Date().getTime(),
