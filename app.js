875b31d70c7314bf8d042d9d148de1abe11f7d57
Martin Zlatkin <mzlatkin@elltechnologies.com>
2018-02-05 23:04:38 -0500

    updated

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ socket.on("connection", function (client) {
     client.on("start_game", function(data){
         console.log("called start game");
         var roster = io.sockets.clients(data["room"]);
-
+        console.log(roster)
         roster.forEach(function(client) {
             console.log('Username: ');
         });
